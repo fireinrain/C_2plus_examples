5d739c3378552233053b2863e5ba002160a5c1bf
lzhaoyang <575563079@qq.com>
2016-12-30 21:30:32 +0800

    c++学习第一次提交

---
+++ typedef_int.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	typedef int int_num;
+	int_num age;
+	int_num height;
+	age = 12;
+	height = 180;
+	cout << "你的年龄是："<<age<<"你的身高是："<<height;
+	return 0; 
+}
