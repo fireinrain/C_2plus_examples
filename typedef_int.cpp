ebf1b51409dbfde9f2ccf46613820cad844551fc
lzhaoyang <575563079@qq.com>
2016-12-30 23:41:08 +0800

    test

--- typedef_int.cpp
+++ typedef_int.cpp
@@ -8,5 +8,12 @@ int main(){
 	age = 12;
 	height = 180;
 	cout << "你的年龄是："<<age<<"你的身高是："<<height;
+	//	定义一个枚举类型
+	enum direction{
+		east,west,south,north
+	} dir;
+	dir = west;
+	cout << "现在的方向是："<<dir; 
 	return 0; 
+	
 }
